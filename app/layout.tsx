72e975571aa0598ded42b2524606ec324d528663
guisantosalves <workguisantos@gmail.com>
2022-12-12 17:35:02 -0400

    making header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import '../styles/globals.css'
+
+// components
+import Header from './Header'
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +11,12 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        <div>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
