5d230eca8c46ada0c97b99363e74d7852fca2c62
guisantosalves <workguisantos@gmail.com>
2022-12-14 11:03:06 -0400

    making page component and creating endpoint in stepzen and graphql

---
+++ lib/fetchNews.ts
@@ -0,0 +1,18 @@
+export const fetchNews = async (
+    category?: Category | string,
+    keyword?: string,
+    isDynamic?: boolean,
+) => {
+    // graphql query
+
+
+    // fetch function with next.js 13 caching...
+    
+    // sort function by images vs not images present
+
+    // return
+}
+
+//stepzen import curl http://api.mediastack.com/v1/news?access_key=b64d6c19045265039705e120adb96737&categories=business,sports
+
+// stepzen import curl http://api.mediastack.com/v1/news?access_key=b64d6c19045265039705e120adb96737&countries=br&limit=100&offset=0&sort=published_desc
--- typings.d.ts
+++ typings.d.ts
@@ -5,9 +5,22 @@ type Pagination = {
   total: Int;
 }
 
+type Article = {
+  author: string | null;
+  category: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+}
+
 type NewsResponse = {
   pagination: Pagination;
-  //data: DataEntry[];
+  data: Article[];
 }
 
 // category can be any of these strings
