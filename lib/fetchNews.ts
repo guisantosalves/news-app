e3a1902de07d966ebcfd7c8892cc4b4cfd43293c
guisantosalves <workguisantos@gmail.com>
2022-12-14 12:55:29 -0400

    setting fetchNews func

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,13 +1,70 @@
+import { gql } from "graphql-request";
+
 export const fetchNews = async (
     category?: Category | string,
     keyword?: string,
     isDynamic?: boolean,
 ) => {
-    // graphql query
 
+    /*
+        generate at stepzen
+    */
+    const query = gql`
+  query MyQuery(
+    $access_key: String!
+    $categories: String!
+    $keywords: String
+  ) {
+    myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "br"
+        sort: "published_desc"
+        keywords: $keywords
+    ) {
+      data {
+        author
+        category
+        description
+        image
+        language
+        published_at
+        source
+        title
+        url
+        country
+      }
+      pagination {
+        count
+        limit
+        offset
+        total
+      }
+    }
+`;
 
-    // fetch function with next.js 13 caching...
-    
+    /*
+        fetch function with next.js 13 caching...
+    */
+    const res = await fetch(`https://niquero.stepzen.net/api/interested-snail/__graphql`, {
+        method: 'POST',
+        cache: isDynamic ? "no-cache" : "default",
+        next: isDynamic ? {revalidate: 0} : {revalidate: 20},
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `APIKey ${process.env.STEPZEN_API_KEY}`
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: {
+                access_key: process.env.MEDIASTACK_API_KEY,
+                categories: category,
+                keywords: keyword
+            }
+        })
+    })
+
+    console.log("Loading new data from api for category", category, keyword)
     // sort function by images vs not images present
 
     // return
